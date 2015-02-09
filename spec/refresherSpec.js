c6c37d261a6a226870bff484b38276e34bd0ad4f
Mishal Islam <mishal.islam01@gmail.com>
2015-02-09 12:57:01 +0000

    first 9 tests passing

--- spec/refresherSpec.js
+++ spec/refresherSpec.js
@@ -1,165 +1,192 @@
-describe('the Friday test :)', function(){
-  
-  it('select_elements_starting_with_a', function(){
-    var n = ['bananas', 'apples', 'pears', 'avocados'].select_elements_starting_with_a 
-    expect(n).toEqual(['apples', 'avocados'])
+describe('JavaScript Refresher Questions', function() {
+
+  it('1. selectElementsStartingWithE: return elements from an array starting with the letter E', function() {
+    testArray = ['emma', 'maya', 'ethan', 'liam', 'elise', 'jackson', 'samaire', ];
+    expect(selectElementsStartingWithE(testArray)).toEqual(['emma', 'ethan', 'elise']);
   });
 
-  it('select_elements_starting_with_vowel', function(){
-    n = ['john', 'david', 'omar', 'fred', 'idris', 'angela'].select_elements_starting_with_vowel
-    expect(n).toEqual(['omar', 'idris', 'angela'])
+  it('2. selectElementsStartingWithVowel: return elements from an array starting with a vowel', function() {
+    testArray = ['open', 'closed', 'small', 'also', 'ice', 'even'];
+    expect(selectElementsStartingWithVowel(testArray)).toEqual(['open', 'also', 'ice', 'even']);
   });
 
-  it('remove_nils_from_array', function(){
-    n = ['a', 'b', nil, nil, false, 'c', nil].remove_nils_from_array 
-    expect(n).toEqual(['a', 'b', false, 'c'])
+  it('3. removeNullsFromArray: remove null elements and return array', function() {
+    testArray = [null, null, true, false, 1, null, 2, 'x', null];
+    expect(removeNullsFromArray(testArray)).toEqual([true, false, 1, 2, 'x']);
   });
 
-  it 'remove_nils_and_false_from_array', function(){
-    n = ['a', 'b', nil, nil, false, 'c', nil].remove_nils_and_false_from_array 
-    expect(n).toEqual(['a', 'b', 'c'])
+  it('4. removeNullsAndFalseFromArray: remove null and false elements and return array', function() {
+    testArray = [null, null, true, false, 1, null, 2, false, 'x', null];
+    expect(removeNullsAndFalseFromArray(testArray)).toEqual([true, 1, 2, 'x']);
   });
 
-  it 'reverse_every_element_in_array', function(){
-    n = ['dog', 'monkey', 'elephant'].reverse_every_element_in_array 
-    expect(n).toEqual(['god', 'yeknom', 'tnahpele'])
+  it('5. reverseEveryElementInArray: return array with each individual string element reversed', function() {
+      testArray = ['coffee', 'tea', 'cake', 'brownie'];
+      expect(reverseEveryElementInArray(testArray)).toEqual(['eeffoc', 'aet', 'ekac', 'einworb']);
   });
 
-  it 'every_possible_pairing_of_students', function(){
-    n = ['Bob', 'Dave', 'Clive'].every_possible_pairing_of_students
-    expect(sorted).toEqual([['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']])
+  it('6. dropFirstThreeElements: return array with first three elements removed', function() {
+    testArray = [1,2,3,4,5,6]
+    expect(dropFirstThreeElements(testArray)).toEqual([4,5,6]);
   });
 
-  it 'all_elements_except_first_3', function(){
-    n = [1, 2, 3, 4, 5, 6, 7].all_elements_except_first_3 
-    expect(n).toEqual([4, 5, 6, 7])
+  it('7. addElementToBeginningOfArray: add element to beginning of an array', function() {
+      testArray = [1,2,3];
+      newElement = 'a';
+      expect(addElementToBeginningOfArray(testArray, newElement)).toEqual(['a',1,2,3]);
   });
 
-  it 'add_element_to_beginning_of_array', function(){
-    n = [2, 3, 4, 5].add_element_to_beginning_of_array(1)
-    expect(n).toEqual([1, 2, 3, 4, 5])
+  it('8. sortArrayByLastLetterOfEachWord: return array sorted by last letter of each string element', function() {
+    testArray = ['oatmeal', 'coffee', 'toast', 'tea'];
+    expect(sortArrayByLastLetterOfEachWord(testArray)).toEqual(['tea', 'coffee', 'oatmeal', 'toast'])
   });
 
-  it 'array_sort_by_last_letter_of_word', function(){
-    n = ['sky', 'puma', 'maker'].array_sort_by_last_letter_of_word 
-    expect(n).toEqual(['puma', 'maker', 'sky'])
+  it('9. returnFirstHalfOfString: return first half of a string', function() {
+      expect(returnFirstHalfOfString('coffee')).toEqual('cof');
+      expect(returnFirstHalfOfString('water')).toEqual('wat');
   });
 
-  it 'get_first_half_of_string', function(){
-    a = 'banana'.get_first_half_of_string 
-    b = 'apple'.get_first_half_of_string 
+  // xit('10. makeNumberNegative: convert positive (or negative) number to a negative number', function() {
+  //   expect(makeNumberNegative(5)).toEqual(-5);
+  //   expect(makeNumberNegative(-3)).toEqual(-3);
+  // });
 
-    expect(a).toEqual('ban')
-    expect(b).toEqual('app')
-  });
+  // xit('11. separateArrayIntoEvenAndOddNumbers: return nested array with one array of even numbers, one array of odd numbers', function() {
+  //   testArray = [1,2,3,4,5,6,7]
+  //   expect(separateArrayIntoEvenAndOddNumbers(testArray)).toEqual([[2,4,6], [1,3,5,7]]);
+  // });
 
-  it 'make_numbers_negative', function(){
-    a = 5.make_numbers_negative 
-    b = -7.make_numbers_negative 
+  // xit('12. numberOfElementsThatArePalindromes: return number of elements in an array that are palindromes', function() {
+  //   testArray = ['pop', 'coffee', 'level', 'hello', 'wow'];
+  //     expect(numberOfElementsThatArePalindromes(testArray)).toEqual(3);
+  // });
 
-    expect(a).toEqual(-5)
-    expect(b).toEqual(-7)
-  });
+  // xit('13. shortestWordInArray: return shortest string in an array', function() {
+  //     testArray = ['muffin', 'scone', 'tea', 'coffee', 'cafe', 'chocolate'];
+  //     expect(shortestWordInArray(testArray)).toEqual('tea');
+  // });
 
-  it 'separate_array_into_even_and_odd_numbers', function(){
-    n = [1, 2, 3, 4, 5, 6, 7].separate_array_into_even_and_odd_numbers 
-    expect(n).toEqual([[2, 4, 6], [1, 3, 5, 7]])
-  });
+  // xit('14. longestWordInArray: return longest string in an array', function() {
+  //   testArray = ['muffin', 'scone', 'tea', 'coffee', 'cafe', 'chocolate'];
+  //   expect(longestWordInArray(testArray)).toEqual('chocolate');
+  // });
 
-  it 'number_of_elements_that_are_palindromes', function(){
-    n = ['bob', 'radar', 'alex', 'noon', 'banana'].number_of_elements_that_are_palindromes 
-    expect(n).toEqual(3)
-  });
+  // xit('15. arrayTotal: return total of number elements in an array', function() {
+  //   testArray = [1,3,5,9,10];
+  //   expect(arrayTotal(testArray)).toEqual(28);
+  // });
 
-  it 'shortest_word_in_array', function(){
-    n = ["here", "is", "a", "bunch", "of", "words", "of", "different", "lengths"].shortest_word_in_array 
-    expect(n).toEqual('a')
-  });
+  // xit('16. doubleArray: return duplicate of array within a single array and NOT sorted', function() {
+  //   testArray = [1,2,3]
+  //   expect(doubleArray(testArray)).toEqual([1,2,3,1,2,3]);
+  // });
 
-  it 'longest_word_in_array', function(){
-    n = ["here", "is", "a", "bunch", "of", "words", "of", "different", "lengths"].longest_word_in_array 
-    expect(n).toEqual('different')
-  });
+  // xit('17. averageOfArray: return average of number elements within an array', function() {
+  //   testArrayA = [8,5,17,24,1,5];
+  //   testArrayB = [5,25,10];
+  //   expect(averageOfArray(testArrayA)).toEqual(10);
+  //   expect(averageOfArray(testArrayB)).toEqual(13.3);
+  // });
 
-  it 'total_of_array', function(){
-    n = [1, 3, 5, 6, 2, 8].total_of_array 
-    expect(n).toEqual(25)
-  });
+  // xit('18. removeElementsGreaterThanFive: return array with elements less than or equal to 5', function() {
+  //   testArray = [1,2,6,8,3,4,1,6,10,5,22,4];
+  //   expect(removeElementsGreaterThanFive(testArray)).toEqual([1,2,3,4,1,5,4]);
+  // });
 
-  it 'double_array', function(){
-    n = [1, 2, 3].double_array 
-    expect(n).toEqual([1, 2, 3, 1, 2, 3])
-  });
+  // xit('19. convertArrayToObject: convert an array into an object with index as the key', function() {
+  //   testArray = ['a','b','c'];
+  //   expect(convertArrayToObject(testArray)).toEqual({0: 'a', 1: 'b', 2:'c'});
+  // });
 
-  it 'turn_symbol_into_string', function(){
-    n = :foobar.turn_symbol_into_string 
-    expect(n).toEqual('foobar')
-  });
+  // xit('20. getLettersInArrayOfWords: return array with letters of all words sorted in alphabetical order', function() {
+  //   testArray = ['hi', 'bye', 'no', 'yes'];
+  //   expect(getLettersInArrayOfWords(testArray)).toEqual(['b','e','e','h','i','n','o','s','y','y'])
+  // });
 
-  it 'average_of_array', function(){
-    n = [10, 15, 25].average_of_array 
-    expect(n).toEqual(17)
-  });
+  // xit('21. swapKeysAndValuesInObject: return an object with key and values switched', function() {
+  //   testObject = {1:'a', 2:'b', 3:'c'};
+  //   expect(swapKeysAndValuesInObject(testObject)).toEqual({'a':1, 'b':2, 'c':3});
+  // });
 
-  it 'get_elements_until_greater_than_five', function(){
-    n = [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7].get_elements_until_greater_than_five 
-    expect(n).toEqual([1, 3, 5, 4, 1, 2])
-  });
+  // xit('22. addKeysAndValues: return sum of numerical keys and values in object', function() {
+  //     testObject = {1:2, 3:4, 5:6}
+  //     expect(addKeysAndValues(testObject)).toEqual(21);
+  // });
 
-  it 'convert_array_to_a_hash', function(){
-    n = ['a', 'b', 'c', 'd'].convert_array_to_an_object
-    expect(n).toEqual({a:'b', c:'d'})
-  });
+  // xit('23. removeCapitalLettersFromString: return string with all capital letters removed', function() {
+  //   testString = 'Coffee Please andThankYou';
+  //   expect(removeCapitalLettersFromString(testString)).toEqual('offee lease andhankou')
+  // });
 
-  it 'remove_capital_letters_from_string', function(){
-    n = 'Hello JohnDoe'.remove_capital_letters_from_string 
-    expect(n).toEqual('ello ohnoe')
-  });
+  // xit('24. everyPossiblePairing: return multidimensional array with every possible pairing and no duplicates', function() {
+  //   testArray = ['Emma', 'Samaire', 'Liam'];
+  //   expect(everyPossiblePairing(testArray)).toEqual([['Emma', 'Samaire'], ['Emma', 'Liam'], ['Samaire', 'Liam']]);
+  // });
 
-  it 'round_up_number', function(){
-    n = 3.142.round_up_number 
-    expect(n).toEqual(4)
-  });
+  // xit('25. roundUpNumber: round all decimal numbers up to the next whole number', function() {
+  //     expect(roundUpNumber(3.5)).toEqual(4);
+  //     expect(roundUpNumber(8.1623)).toEqual(9);
+  // });
 
-  it 'round_down_number', function(){
-    n = 4.9.round_down_number 
-    expect(n).toEqual(4)
-  });
+  // xit('26. roundDownNumber: round all decimal numbers down to the previous whole number', function() {
+  //   expect(roundDownNumber(3.5)).toEqual(3);
+  //   expect(roundDownNumber(8.1623)).toEqual(8);
+  // });
 
-  it 'format_date_nicely', function(){
-    n = new Date(2013, 10, 31).format_date_nicely 
-    expect(n).toEqual('31/10/2013')
-  });
+  // xit('27. formatDateNicely: format date in a user-friendly format', function() {
+  //   date = new Date(2015, 02, 24)
+  //   expect(formatDateNicely(date)).toEqual('24-Mar-2015');
+  // });
 
-  it 'titleize_a_string', function(){
-    n = titleize_a_string 'the lion the witch and the wardrobe'
-    expect(n).toEqual('The Lion the Witch and the Wardrobe')
-  });
+  // xit('28. getDomainNameFromEmailAddress: return domain name of email address without @ or .com', function() {
+  //   emailA = 'test@emailprovider.com';
+  //   emailB = 'test@emailme.co.uk'
+  //   expect(getDomainNameFromEmailAddress(emailA)).toEqual('emailprovider');
+  //   expect(getDomainNameFromEmailAddress(emailB)).toEqual('emailme');
+  // });
 
-  it 'check_a_string_for_special_characters', function(){
-    a = 'ABCdef123'.check_a_string_for_special_characters 
-    b = 'ABC@def123!'.check_a_string_for_special_characters 
-    expect(a).toBe(false)
-    expect(b).toBe(true)
-  });
+  // xit('29. titleizeString: return string with first letter of each word capitalised, except for certain words (and, in, the, of, a, an, to)', function() {
+  //   testString = 'the definitive guide to international relations in the 21st century';
+  //   expect(titleizeString(testString)).toEqual('The Definitive Guide To International Relations In The 21st Century');
+  // });
 
-  it 'square_root_of', function(){
-    a = 9.square_root
-    b = 3.square_root
-    expect(a).toEqual(3.0)
-    expect(b).toEqual(1.7320508075688772)
-  });
+  // xit('30. checkStringForSpecialCharacters: return true or false if string contains non-alphanumerical characters', function() {
+  //   stringA = 'abcd1234'
+  //   stringB = 'abcd1234!@#$'
+  //   expect(checkStringForSpecialCharacters(stringA)).toBe(false);
+  //   expect(checkStringForSpecialCharacters(stringB)).toBe(true);
+  // });
 
-  it 'is_a_2014_bank_holiday?', function(){
-    a = new Date(2014, 8, 25).is_a_2014_bank_holiday
-    b = new Date(2014, 8, 26).is_a_2014_bank_holiday
-    expect(a).toBe(true)
-    expect(b).toBe(false)
-  });
+  // xit('31. getUpperLimitOfRange: return max value of pre-determined range of numbers', function() {
+  //   range = Array.apply(null, Array(25)).map(function (_, i) {return i+1;});
+  //   expect(getUpperLimitOfRange(range)).toEqual(25);
+  // });
 
-  it 'your_birthday_is_on_a_friday_in_the_year', function(){
-    n = new Date(2013, 1, 1).your_birthday_is_on_a_friday_in_the_year
-    expect(n).toEqual(2016)
-  });
+  // xit('32. squareRootOf: return the square root of a given number', function() {
+  //   numberA = 25;
+  //   numberB = 75;
+  //   expect(squareRootOf(25)).toEqual(5);
+  //   expect(squareRootOf(75)).toEqual(8.660254037844387);
+  // });
+
+  // xit('33. callMethodFromString: allows user to call a method using the method name converted to a string', function() {
+  //   expect(callMethodFromString('sayHello')).toEqual('Hello!');
+  // })
+
+  // xit('34. isA2015BankHoliday: returns true if a date is a 2015 Bank holiday', function() {
+  //   dateA = new Date(2015, 05, 25);
+  //   dateB = new Date(2015, 02, 06 );
+  //   expect(isA2015BankHoliday(dateA)).toEqual(true);
+  //   expect(isA2015BankHoliday(dateB)).toEqual(false);
+  // });
+
+  // xit('35. convertToRomanNumerals: returns number from 0 to 99 in roman numeral format', function() {
+  //   numberA = 6;
+  //   numberB = 25;
+  //   numberC = 87;
+  //   expect(convertToRomanNumerals(numberA)).toEqual('VI');
+  //   expect(convertToRomanNumerals(numberB)).toEqual('XXV');
+  //   expect(convertToRomanNumerals(numberC)).toEqual('LXXXVII')
+  // });
 
 });
